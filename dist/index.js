d981a3887d64a8abbb04721ff326aaabb7d641e1
Kieve Chua <kievechua@gmail.com>
2017-03-23 14:47:04 +0800

    Update build

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,23 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const module_1 = require("magnet-core/module");
 const Sequelize = require("sequelize");
-const convert_1 = require("magnet-core/convert");
-const sequelize_1 = require("./config/sequelize");
-exports.default = convert_1.default(Sequelize, {
-    namespace: 'sequelize',
-    params: ['config.database', 'config.user', 'config.password', 'config']
-}, sequelize_1.default);
+class MagnetSequelize extends module_1.Module {
+    get moduleName() { return 'sequelize'; }
+    get defaultConfig() { return __dirname; }
+    setup() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.insert(new Sequelize(this.config.database, this.config.user, this.config.password, this.config));
+        });
+    }
+}
+exports.default = MagnetSequelize;
 //# sourceMappingURL=index.js.map
